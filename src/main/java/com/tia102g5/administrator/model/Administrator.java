b86d1fdbbcfeeae6971725fa178f49e2d7980ba1
Luke <kai199202232578@gmail.com>
2024-07-28 11:52:50 +0800

    建立廠商會員及管理員VO

---
+++ src/main/java/com/tia102g5/administrator/model/Administrator.java
@@ -0,0 +1,89 @@
+package com.tia102g5.administrator.model;
+
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "Administrator")
+public class Administrator {
+
+	
+	
+	@Column(name = "administratorID", updatable = false) // "管理員ID"
+	private Integer administratorID;
+	
+	@Column(name = "administratorAccount") // "帳號"
+	private String administratorAccount;
+	
+	@Column(name = "administratorPassword") // "密碼"
+	private String administratorPassword;
+	
+	@Column(name = "administratorCreateTime", updatable = false) // "帳號建立時間"
+	private Date administratorCreateTime;
+	
+	@Column(name = "administratorStatus") // "帳號狀態 0:帳號正常 1:帳號停用"
+	private Integer administratorStatus;
+
+	public Administrator() {
+		super();
+	}
+
+	public Administrator(Integer administratorID, String administratorAccount, String administratorPassword,
+			Date administratorCreateTime, Integer administratorStatus) {
+		super();
+		this.administratorID = administratorID;
+		this.administratorAccount = administratorAccount;
+		this.administratorPassword = administratorPassword;
+		this.administratorCreateTime = administratorCreateTime;
+		this.administratorStatus = administratorStatus;
+	}
+
+	public Integer getAdministratorID() {
+		return administratorID;
+	}
+
+	public void setAdministratorID(Integer administratorID) {
+		this.administratorID = administratorID;
+	}
+
+	public String getAdministratorAccount() {
+		return administratorAccount;
+	}
+
+	public void setAdministratorAccount(String administratorAccount) {
+		this.administratorAccount = administratorAccount;
+	}
+
+	public String getAdministratorPassword() {
+		return administratorPassword;
+	}
+
+	public void setAdministratorPassword(String administratorPassword) {
+		this.administratorPassword = administratorPassword;
+	}
+
+	public Date getAdministratorCreateTime() {
+		return administratorCreateTime;
+	}
+
+	public Integer getAdministratorStatus() {
+		return administratorStatus;
+	}
+
+	public void setAdministratorStatus(Integer administratorStatus) {
+		this.administratorStatus = administratorStatus;
+	}
+
+	@Override
+	public String toString() {
+		return "Administrator [administratorID=" + administratorID + ", administratorAccount=" + administratorAccount
+				+ ", administratorPassword=" + administratorPassword + ", administratorCreateTime="
+				+ administratorCreateTime + ", administratorStatus=" + administratorStatus + "]";
+	}
+	
+	
+}
---
+++ src/main/java/com/tia102g5/partnermember/model/PartnerMember.java
@@ -0,0 +1,153 @@
+package com.tia102g5.partnermember.model;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "PartnerMember")
+public class PartnerMember {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "partnerID", updatable = false) // "廠商ID"
+	private Integer partnerID;
+	
+	@Column(name = "taxID") // "統一編號(登入帳號)"
+	private String taxID;
+	
+	@Column(name = "partnerName") // "公司名稱"
+	private String partnerName;
+	
+	@Column(name = "partnerHeading") // "抬頭"
+	private String partnerHeading;
+	
+	@Column(name = "partnerAddress") // "地址"
+	private String partnerAddress;
+	
+	@Column(name = "partnerPhone") // "連絡電話"
+	private String partnerPhone;
+	
+	@Column(name = "partnerContactPerson") // "聯絡人"
+	private String partnerContactPerson;
+	
+	@Column(name = "partnerPassword") // "密碼"
+	private String partnerPassword;
+	
+	@Column(name = "partnerEmail") // "電子信箱"
+	private String partnerEmail;
+	
+	
+	@Column(name = "partnerCreateTime", updatable = false) // "帳號建立時間"
+	private Date partnerCreateTime;
+	
+	@Column(name = "partnerAccountStatus") // "帳號狀態 0:黑名單 1.使用中 2.申請中"
+	private Integer partnerAccountStatus;
+
+	public Integer getPartnerID() {
+		return partnerID;
+	}
+
+	public void setPartnerID(Integer partnerID) {
+		this.partnerID = partnerID;
+	}
+
+	public String getTaxID() {
+		return taxID;
+	}
+
+	public void setTaxID(String taxID) {
+		this.taxID = taxID;
+	}
+
+	public String getPartnerName() {
+		return partnerName;
+	}
+
+	public void setPartnerName(String partnerName) {
+		this.partnerName = partnerName;
+	}
+
+	public String getPartnerHeading() {
+		return partnerHeading;
+	}
+
+	public void setPartnerHeading(String partnerHeading) {
+		this.partnerHeading = partnerHeading;
+	}
+
+	public String getPartnerAddress() {
+		return partnerAddress;
+	}
+
+	public void setPartnerAddress(String partnerAddress) {
+		this.partnerAddress = partnerAddress;
+	}
+
+	public String getPartnerPhone() {
+		return partnerPhone;
+	}
+
+	public void setPartnerPhone(String partnerPhone) {
+		this.partnerPhone = partnerPhone;
+	}
+
+	public String getPartnerContactPerson() {
+		return partnerContactPerson;
+	}
+
+	public void setPartnerContactPerson(String partnerContactPerson) {
+		this.partnerContactPerson = partnerContactPerson;
+	}
+
+	public String getPartnerPassword() {
+		return partnerPassword;
+	}
+
+	public void setPartnerPassword(String partnerPassword) {
+		this.partnerPassword = partnerPassword;
+	}
+
+	public String getPartnerEmail() {
+		return partnerEmail;
+	}
+
+	public void setPartnerEmail(String partnerEmail) {
+		this.partnerEmail = partnerEmail;
+	}
+
+	public Date getPartnerCreateTime() {
+		return partnerCreateTime;
+	}
+	
+
+	public void setPartnerCreateTime(Date partnerCreateTime) {
+		this.partnerCreateTime = partnerCreateTime;
+	}
+
+	public Integer getPartnerAccountStatus() {
+		return partnerAccountStatus;
+	}
+
+	public void setPartnerAccountStatus(Integer partnerAccountStatus) {
+		this.partnerAccountStatus = partnerAccountStatus;
+	}
+	
+	
+
+	@Override
+	public String toString() {
+		return "Partner [partnerID=" + partnerID + ", taxID=" + taxID + ", partnerName=" + partnerName
+				+ ", partnerHeading=" + partnerHeading + ", partnerAddress=" + partnerAddress + ", partnerPhone="
+				+ partnerPhone + ", partnerContactPerson=" + partnerContactPerson + ", partnerPassword="
+				+ partnerPassword + ", partnerEmail=" + partnerEmail + ", partnerCreateTime=" + partnerCreateTime
+				+ ", partnerAccountStatus=" + partnerAccountStatus + "]";
+	}
+	
+	
+}
