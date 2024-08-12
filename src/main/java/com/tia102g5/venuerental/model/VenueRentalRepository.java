e868f38575b591a97b38af79545c0c00f4cc4886
kao <hanskao777@gmail.com>
2024-08-12 19:01:33 +0800

    Add partnerVenueRental pages,venueIntroduction page.Adjust sidebar.js and sidebar.html

--- src/main/java/com/tia102g5/venuerental/model/VenueRentalRepository.java
+++ src/main/java/com/tia102g5/venuerental/model/VenueRentalRepository.java
@@ -1,7 +1,10 @@
 package com.tia102g5.venuerental.model;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface VenueRentalRepository extends JpaRepository<VenueRental,Integer>{
-
+	
+	 List<VenueRental> findByPartnerMemberPartnerID(Integer partnerID);
 }
