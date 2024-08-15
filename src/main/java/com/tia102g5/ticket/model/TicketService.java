d4ad914e0f0b93f555efd6f8c03bca881fda49ba
maddogbitesme <koenigsegg1995@gmail.com>
2024-08-15 13:35:11 +0800

    Updated for SQL script in CommodityPicture

---
+++ src/main/java/com/tia102g5/ticket/model/TicketService.java
@@ -0,0 +1,53 @@
+package com.tia102g5.ticket.model;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service("ticketService")
+public class TicketService {
+
+		@Autowired
+		TicketRepository repository;
+		
+		@Autowired
+		private SessionFactory sessionFactory;
+		
+		//新增
+		public void addTicket(Ticket ticket) {
+			repository.save(ticket);
+		}
+		
+		//修改
+		public void updateTicket(Ticket ticket) {
+			repository.save(ticket);
+		}
+		
+		//刪除
+		public void deleteTicket(Integer ticketID) {
+			if(repository.existsById(ticketID)) {
+				repository.deleteByTicketID(ticketID);
+			}
+		}
+		
+		//查詢 (單一)
+		public Ticket getOneTicket(Integer ticketID) {
+			Optional<Ticket> optional = repository.findById(ticketID);
+			
+			return optional.orElse(null);
+		}
+		
+		//查詢 (全部)
+		public List<Ticket> getAll(){
+			return repository.findAll();
+		}
+		
+//		//查詢 (複合)
+//		public List<Ticket> getAll(Map<String, String[]> map) {
+//			return HibernateUtil_CompositeQuery_Ticket.getAllC(map,sessionFactory.openSession());
+//		}
+		
+}
