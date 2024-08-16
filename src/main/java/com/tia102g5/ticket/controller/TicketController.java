9c5d1c33c61ba61bdb157d4d8df26977950d1c6a
maddogbitesme <koenigsegg1995@gmail.com>
2024-08-16 22:19:40 +0800

    Updated for bookTicket

--- src/main/java/com/tia102g5/ticket/controller/TicketController.java
+++ src/main/java/com/tia102g5/ticket/controller/TicketController.java
@@ -2,12 +2,17 @@ package com.tia102g5.ticket.controller;
 
 import java.util.List;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.tia102g5.activity.model.Activity;
 import com.tia102g5.activity.model.ActivityService;
@@ -27,7 +32,7 @@ public class TicketController {
 /********************* 跳轉 **********************/
 //////////////// 前台 ////////////////
 	//票券結帳
-	@GetMapping("/bookTicket")
+	@GetMapping("bookTicket")
 	public String bookTicket() {
 		return "front-end/ticket/bookTicket";
 	}
@@ -35,16 +40,29 @@ public class TicketController {
 	
 //////////////// 後台 ////////////////
 	//售票資訊
-	@GetMapping("/ticketDisplay")
+	@GetMapping("ticketDisplay")
 	public String ticketDisplay() {
 		return "back-end-partner/ticket/ticketDisplay";
 	}
 //////////////// 後台 ////////////////
 /********************* 跳轉 **********************/
 	
-/********************* 跳轉 **********************/
-	
-/********************* 跳轉 **********************/
+/********************* action **********************/
+	//刪減票券
+	@PostMapping("deleteOneTicket")
+	public String deleteOneTicket(@RequestParam("count") Integer count, HttpSession session, ModelMap model) {
+		List<Ticket> ticketList = (List<Ticket>)session.getAttribute("ticketList");
+		ticketList.remove(count - 1);
+		
+		if(ticketList.isEmpty()) {
+			return "/";
+		}
+		
+		session.setAttribute("ticketList", ticketList);
+		
+		return "redirect:/ticket/bookTicket";
+	}
+/********************* action **********************/
 	
 /********************* bean **********************/
 	//查全部，給 ticketDisplay 用
@@ -59,10 +77,6 @@ public class TicketController {
 	@ModelAttribute("ticketListData")
 	protected List<Ticket> referenceTicketListData(Model model) {
 		List<Ticket> list = ticketSvc.getAll();
-		System.out.println("------------------------------------------");
-		System.out.println(list);
-		System.out.println("------------------------------------------");
-		
 		return list;
 	}
 /********************* bean **********************/
