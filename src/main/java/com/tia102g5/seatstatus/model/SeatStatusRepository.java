58afb6d084134987f32d0722c453b956bd9c269d
kao <hanskao777@gmail.com>
2024-08-17 23:07:00 +0800

    Add show seatStatus

--- src/main/java/com/tia102g5/seatstatus/model/SeatStatusRepository.java
+++ src/main/java/com/tia102g5/seatstatus/model/SeatStatusRepository.java
@@ -1,5 +1,7 @@
 package com.tia102g5.seatstatus.model;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -10,4 +12,13 @@ public interface SeatStatusRepository extends JpaRepository<SeatStatus, Integer>
 	@Query(value = "SELECT * FROM seatStatus " + "WHERE activityTimeSlotID = ?1 "
 			+ "AND seatID = ?2", nativeQuery = true)
 	SeatStatus findSeatStatusByActivityTimeSlotIdAndSeatId(Integer activityTimeSlotId, Integer seatId);
+
+	@Query(value = "SELECT * FROM seatstatus WHERE activityTimeSlotID = ?1", nativeQuery = true)
+	List<SeatStatus> findAllByActivityTimeSlotID(Integer activityTimeSlotID);
+
+	//為了在seatSelectPage顯示不可選取的座位而寫的方法)
+	@Query(value = "SELECT s.seatName FROM seat s " + "JOIN seatStatus ss ON s.seatID = ss.seatID "
+			+ "WHERE ss.activityTimeSlotID = ?1 " + "AND ss.seatStatus IN (?2, ?3)", nativeQuery = true)
+	List<String> findUnavailableSeatNamesByActivityTimeSlotID(Integer activityTimeSlotID, Integer status1,
+			Integer status2);
 }
